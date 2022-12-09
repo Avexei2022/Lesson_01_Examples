2381a34db89b4164e29f9c148184e5f4d5d4109b
alex123 <alex123@dot.com>
2022-12-09 18:08:17 +0500

    add examle006

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿// А program for solving problems with kettlebell
+Console.WriteLine("Решаем задачку с гирьками");
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("Самая тяжелая гирька весом (кг): ");
+Console.WriteLine(max);
+
