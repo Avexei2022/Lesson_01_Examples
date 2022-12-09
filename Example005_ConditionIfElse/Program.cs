c2be0b9da0d7ffa2ca72ae1e2e70266b07d4a006
alex123 <alex123@dot.com>
2022-12-09 17:29:06 +0500

    final code for examle005

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,13 @@
 ﻿// Personal greeting code
-Console.Write("Введите имя пользователя:");
-string username=Console.ReadLine();
-if(username=="Маша")
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
 {
-Console.WriteLine("Ура, это же Маша");
+    Console.WriteLine("Я ждал тебя, Маша!");
 }
 else
 {
     Console.Write("Привет, ");
-    Console.WrteLine(username);
+    Console.Write(username);
+    Console.WriteLine("!");
 }
