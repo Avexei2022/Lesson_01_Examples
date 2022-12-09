43bffe7eccf6170bb890bbc090ea37eccc64c9a6
alex123 <alex123@dot.com>
2022-12-09 16:12:37 +0500

    fix sintaksis mistakes at example003_sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -2,6 +2,6 @@
 int numberA = new Random().Next(1, 10);  // 1 2 3 4 ...9
 Console.WriteLine(numberA);
 int numberВ = new Random().Next(1, 10);
-Console.WriteLine(numberB);
+Console.WriteLine(numberВ);
 int result = numberA + numberВ;
 Console.WriteLine(result);
