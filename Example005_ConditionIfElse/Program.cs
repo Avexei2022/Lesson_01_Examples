6f76f2684058297b1ef101421c5808071ec944b9
alex123 <alex123@dot.com>
2022-12-09 17:06:23 +0500

    add first code into example005_cinditionifelse

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Personal greeting code
+Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+Console.WriteLine(username);
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет,с");
+    Console.WrteLine(username)
+}
