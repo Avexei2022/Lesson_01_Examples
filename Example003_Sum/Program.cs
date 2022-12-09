ce66255189c49c8773cde0c8bfa9d6e3b7e3ca23
alex123 <alex123@dot.com>
2022-12-09 16:07:14 +0500

    again add random numbers with writeline at example003_sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Sum two integer
 int numberA = new Random().Next(1, 10);  // 1 2 3 4 ...9
+Console.WriteLine(numberA);
 int numberВ = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberВ;
 Console.WriteLine(result);
