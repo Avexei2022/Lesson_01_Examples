7669804682a8931d494df027a2080a09a139e9f8
alex123 <alex123@dot.com>
2022-12-14 19:27:51 +0500

    Добавили Example008_Max"

---
+++ Example008_Max/Program.cs
@@ -0,0 +1,43 @@
+﻿// Максимум из 9 чисел
+Console.Write("Введите значение: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение: ");
+int d = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение: ");
+int e = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение: ");
+int f = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение: ");
+int g = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение: ");
+int h = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+if (max < f) max = f;
+if (max < g) max = g;
+if (max < h) max = h;
+if (max < i) max = i;
+
+Console.Write("max = ");
+Console.Write(max);
+
+
